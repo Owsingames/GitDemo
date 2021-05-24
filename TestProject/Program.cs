13315613208336cd117865fdc78d89a0d4701af3
james <owsingames@yahoo.com>
2021-05-24 11:19:39 -0400

    added pick number

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -9,6 +9,11 @@ namespace TestProject
             Console.WriteLine("Hello World!");
             Console.WriteLine("Test first git repo");
             Console.WriteLine("change");
+
+            Console.WriteLine("Please pick a number between 1 and 10");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("You picked: " + input);
         }
     }
 }
